3bb6977b375d11cc921714ca842248490f8e2fd9
developerashikur1 <masik4506@gmail.com>
2023-02-15 22:42:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+// import VANTA from "vanta/dist/vanta.birds.min.js";
+import BIRDS from 'vanta/dist/vanta.birds.min';
 import './App.css';
 
+window.THREE = THREE;
+
 function App() {
+
+const vantaRef = useRef(null);
+
+  useEffect(() => {
+    const birdBackground = BIRDS({
+      el: vantaRef.current,
+      THREE: THREE,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0x0,
+      color1: 0x1f00ff,
+      color2: 0xfaed00,
+      colorMode: "lerp",
+      birdSize: 1.90,
+      wingSpan: 18.00,
+      separation: 45.00,
+      alignment: 45.00,
+      cohesion: 1.00
+    });
+
+    return () => {
+      if (birdBackground) {
+        birdBackground.destroy();
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <>
+            <div id="vanta-net" ref={vantaRef}>
+                
+            </div>
+            <div className="vanta-content">
+                <div>
+                    <h1>{`Vanta.js -> Karan patel media`}</h1>
+                    <p>Coming soon</p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
